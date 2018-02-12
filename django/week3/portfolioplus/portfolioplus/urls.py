bf9ef6d1161699be226d84dac6533d060a3b7fdf
Ryan Warrick <rwarrick@rwarricks-mba.local>
2018-02-11 19:39:32 -0500

    Adding home page to URL and views.

--- django/week3/portfolioplus/portfolioplus/urls.py
+++ django/week3/portfolioplus/portfolioplus/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(r'home', views.home, name='home'),
+    path(r'admin', admin.site.urls),
 ]
