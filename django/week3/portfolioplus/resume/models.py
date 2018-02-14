f944949b1c1877db0682e58adefe87a22c508187
Ryan Warrick <rwarrick@rwarricks-mba.aw4.unh.edu>
2018-02-13 20:57:21 -0500

    Remigrating to fix Models issues

--- django/week3/portfolioplus/resume/models.py
+++ django/week3/portfolioplus/resume/models.py
@@ -5,19 +5,19 @@ from django.db import models
 class Experience(models.Model):
     title = models.CharField(max_length=64, null=False, blank=True)
     location = models.CharField(max_length=255, null=False, blank=True)
-    start_date = models.CharField(max_length=255, null=False, blank=True)
-    end_date = models.CharField(max_length=355, null=False, blank=True)
+    start_date = models.DateField(max_length=255, null=False, blank=True)
+    end_date = models.DateField(max_length=355, null=False, blank=True)
     description = models.TextField(max_length=255, null=False, blank=True)
 
     def new_experience(self):
-        return sel.new_experience
+        return self.new_experience
 
 class Education(models.Model):
     institution_name = models.CharField(max_length=64, null=False, blank=True)
     location = models.CharField(max_length=255, null=False, blank=True)
     degree = models.CharField(max_length=255, null=False, blank=True)
     major = models.CharField(max_length=355, null=False, blank=True)
-    gpa = models.TextField(max_length=255, null=False, blank=True)
+    gpa = models.FloatField()
 
     def new_education(self):
         return self.new_education
